fdd87300dbf8d9d2c70497ccdf310ce3eb8ef1b9
Chris McQueen <c.mcqueen@newcastle.ac.uk>
2018-03-23 10:01:10 +0000

    LED is now based on the light sensor

--- driverPi.py
+++ driverPi.py
@@ -1,14 +1,27 @@
 from grovepi import *
+import time
+import grovepi
 
 led = 2
 pinMode(led,"OUTPUT")
 
+light_sensor = 0
+threshold = 10
+grovepi.pinMode(light_sensor,"INPUT")
+
+
 while True:
-    try:
-        digitalWrite(led,1)
-        
-    except KeyboardInterrupt:
-        digitalWrite(led,0)
-        break
-    except IOError:				
-        print ("Error")
+    sensor_value = grovepi.analogRead(light_sensor)
+
+        # Calculate resistance of sensor in K
+    resistance = (float)(1023 - sensor_value) * 10 / sensor_value
+
+    if resistance > threshold:
+            # Send HIGH to switch on LED
+        grovepi.digitalWrite(led,1)
+    else:
+            # Send LOW to switch off LED
+        grovepi.digitalWrite(led,0)
+
+    print("sensor_value = %d resistance = %.2f" %(sensor_value,  resistance))
+    time.sleep(.5)
