1551db31a5a3bbb78dcc5f3a5717a5a1869f3477
Chris McQueen <c.mcqueen@newcastle.ac.uk>
2018-03-23 10:59:37 +0000

    updates free seats on close proximity

--- driverPi.py
+++ driverPi.py
@@ -1,6 +1,9 @@
 from grovepi import *
 import time
 import grovepi
+import requests
+import json
+
 
 led = 2
 pinMode(led,"OUTPUT")
@@ -9,7 +12,7 @@ light_sensor = 0
 threshold = 10
 grovepi.pinMode(light_sensor,"INPUT")
 
-
+numberOfFreeSeats = 3;
 while True:
     sensor_value = grovepi.analogRead(light_sensor)
 
@@ -18,10 +21,20 @@ while True:
 
     if resistance > threshold:
             # Send HIGH to switch on LED
+        r = requests.get('https://school-run-451c2.firebaseio.com/schools.json')
+        #data = r.json()
+        payload = r.json()
+        currentFreeSeats = payload['jesmond-high']['spaces']
+        newFreeSeats = currentFreeSeats + numberOfFreeSeats
+        
+        payload['jesmond-high']['spaces'] = newFreeSeats
+        print(json.dumps(payload))
+        
+        put = requests.put("https://school-run-451c2.firebaseio.com/schools.json", data=json.dumps(payload))
+        print(put.text)
         grovepi.digitalWrite(led,1)
     else:
             # Send LOW to switch off LED
         grovepi.digitalWrite(led,0)
-
-    print("sensor_value = %d resistance = %.2f" %(sensor_value,  resistance))
-    time.sleep(.5)
+        print("sensor_value = %d resistance = %.2f" %(sensor_value,  resistance))
+        time.sleep(.2)
